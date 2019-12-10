72fa64f577b64151776fff8d1934fb244467f207
Peter Gagliardi <ptrgags@gmail.com>
2019-12-09 21:44:46 -0500

    Sliceform proof-of-concept

--- src/main.rs
+++ src/main.rs
@@ -34,13 +34,100 @@ fn intervals(left: f64, right: f64, depth: u32, max_depth: u32) -> Vec<f64> {
     result
 }
 
+type Surface = fn(f64, f64) -> f64;
+
+fn product(x: f64, y: f64) -> f64 {
+    x * y
+}
+
+fn x_slices(surf: Surface, slice_resolution: u32, curve_resolution: u32) 
+        -> Vec<Vec<Polyline>> {
+    make_intervals(slice_resolution, false).into_iter().map(|y0| {
+        x_slice(surf, y0, slice_resolution, curve_resolution) 
+    }).collect()
+}
+
+fn x_slice(
+        surf: Surface, y0: f64, slice_resolution: u32, curve_resolution: u32) 
+        -> Vec<Polyline> {
+    let mut outline_vertices = vec![Vec2(0.0, 0.0), Vec2(1.0, 0.0)]; 
+    for x in make_intervals(curve_resolution, true).into_iter().rev() {
+        let height = surf(x, y0);
+        outline_vertices.push(Vec2(x, height));
+    }
+    let outline = Polyline::new(&outline_vertices, true);
+
+    let mut slits = Vec::new();
+    for x in make_intervals(slice_resolution, false).into_iter() {
+        let height = surf(x, y0);
+        let slit = Polyline::new(&[
+            Vec2(x, height),
+            Vec2(x, height / 2.0)
+        ], false);
+
+        slits.push(slit);
+    }
+
+    let mut result = vec![outline];
+    result.append(&mut slits);
+
+    result
+}
+
+fn y_slices(surf: Surface, slice_resolution: u32, curve_resolution: u32) 
+        -> Vec<Vec<Polyline>> {
+    make_intervals(slice_resolution, false).into_iter().map(|x0| {
+        y_slice(surf, x0, slice_resolution, curve_resolution) 
+    }).collect()
+}
+
+fn y_slice(
+        surf: Surface, x0: f64, slice_resolution: u32, curve_resolution: u32) 
+        -> Vec<Polyline> {
+    let mut outline_vertices = vec![Vec2(0.0, 0.0), Vec2(1.0, 0.0)]; 
+    for y in make_intervals(curve_resolution, true).into_iter() {
+        let height = surf(x0, y);
+        outline_vertices.push(Vec2(1.0 - y, height));
+    }
+    let outline = Polyline::new(&outline_vertices, true);
+
+    let mut slits = Vec::new();
+    for y in make_intervals(slice_resolution, false).into_iter() {
+        let height = surf(x0, y);
+        let slit = Polyline::new(&[
+            Vec2(1.0 - y, 0.0),
+            Vec2(1.0 - y, height / 2.0)
+        ], false);
+
+        slits.push(slit);
+    }
+
+    let mut result = vec![outline];
+    result.append(&mut slits);
+
+    result
+}
+
 fn main() {
-    println!("{:?}", make_intervals(3, true));
-    println!("{:?}", make_intervals(3, false));
-
-    let points = vec![Vec2(10.0, 10.0), Vec2(100.0, 10.0), Vec2(100.0, 100.0)];
-    let poly = Polyline::new(&points, false);
-
     let mut file = File::create("test.ps").expect("Could not open file");
-    poly.write_postscript(&mut file);
+
+    let slices = x_slices(product, 1, 6);
+    let dpi = 72.0;
+    let margin = dpi / 2.0;
+    for (i, slice) in slices.into_iter().enumerate() {
+        let translate = Vec2(margin, (i as f64) * 2.0 * dpi + margin);
+        for poly in slice {
+            poly.transform(2.0 * dpi, translate).write_postscript(&mut file);
+        }
+    }
+
+    let slices = y_slices(product, 1, 6);
+    let dpi = 72.0;
+    let margin = dpi / 2.0;
+    for (i, slice) in slices.into_iter().enumerate() {
+        let translate = Vec2(3.0 * dpi + margin, (i as f64) * 2.0 * dpi + margin);
+        for poly in slice {
+            poly.transform(2.0 * dpi, translate).write_postscript(&mut file);
+        }
+    }
 }
