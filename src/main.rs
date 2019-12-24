3750a80ffe55db85fd05d861474e417e9076df81
Peter Gagliardi <ptrgags@gmail.com>
2019-12-23 19:55:28 -0500

    tweak size

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,6 @@ fn main() {
     let surf = models::select_model("sine_hill");
     let slicer = Slicer::new(2, 6, surf);
 
-    let mut printer = Printer::new("slicetest.ps", 2.5, false);
+    let mut printer = Printer::new("slicetest.ps", 3.5, false);
     printer.print_slices(&slicer);
 }
